66a267e553038abe99b19c56846081050c8e8b5e
Thiru-5 <165800485+Thiru-5@users.noreply.github.com>
2025-05-13 15:38:23 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (54.10%) - LeetSync

--- 2580-circular-sentence/circular-sentence.py
+++ 2580-circular-sentence/circular-sentence.py
@@ -4,7 +4,9 @@ class Solution:
         if len(a)==1:
             if a[0][0] != a[0][-1]:
                 return False
+        if a[0][0]!=a[-1][-1]:
+            return False
         for i in range(len(a)-1):
-            if a[0][0]!=a[-1][-1] or a[i][-1]!=a[i+1][0]:
+            if a[i][-1]!=a[i+1][0]:
                 return (False)
         return (True) 
