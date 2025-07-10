1acb6ba241e1e19cda2509d2b03bc0f3433a8236
Thiru-5 <165800485+Thiru-5@users.noreply.github.com>
2025-07-10 12:35:39 +0530

    Time: 1185 ms (6.78%) | Memory: 21.1 MB (89.94%) - LeetSync

---
+++ 835-linked-list-components/linked-list-components.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
+        a=0
+        while head:
+            if (head.val in nums) and (not head.next or head.next.val not in nums):
+                a+=1
+            head=head.next
+        return a
