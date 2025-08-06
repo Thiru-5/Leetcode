f3d67a7a076072cb3658f02480f625dc03d69732
Thiru-5 <165800485+Thiru-5@users.noreply.github.com>
2025-08-06 09:32:16 +0530

    Time: 1 ms (52.91%) | Memory: 17.8 MB (69.24%) - LeetSync

--- 3324-split-the-array/split-the-array.py
+++ 3324-split-the-array/split-the-array.py
@@ -1,6 +1,11 @@
 class Solution:
     def isPossibleToSplit(self, nums: List[int]) -> bool:
-        count = Counter(nums)
-        if max(count.values()) >= 3:
-            return False
+        nums.sort()
+        a=[]
+        b=[]
+        for i in range(0,len(nums),2):
+            if nums[i] in a or nums[i+1] in b:
+                return False
+            a.append(nums[i])
+            b.append(nums[i+1])
         return True
