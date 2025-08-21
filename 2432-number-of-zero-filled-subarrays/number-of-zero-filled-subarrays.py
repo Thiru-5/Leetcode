88202cb3025b1f6f8ce0287470cf5129c0fd4fac
Thiru-5 <165800485+Thiru-5@users.noreply.github.com>
2025-08-21 09:27:14 +0530

    Time: 24 ms (92.26%) | Memory: 28.4 MB (81.63%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
@@ -1,19 +1,13 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
-        res=[]
-        c=0
+        a=b=0
         for i in nums:
             if i==0:
-                c+=1
-            else:
-                a=c
-                b=a*(a+1)//2
-                res.append(b)
-                c=0
-        a=c
-        b=a*(a+1)//2
-        res.append(b)
-        
-        return sum(res)
+                a+=1
+            elif a:
+                b+=(a*(a+1))//2
+                a=0
+        b+=(a*(a+1))//2        
+        return b
 
         
