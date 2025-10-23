a547802e4c8e70ab4fdbc5ebbe118bda9327a387
Thiru-5 <165800485+Thiru-5@users.noreply.github.com>
2025-10-23 19:53:13 +0530

    Time: 3 ms (39.80%) | Memory: 17.9 MB (38.74%) - LeetSync

--- 3994-find-the-least-frequent-digit/find-the-least-frequent-digit.py
+++ 3994-find-the-least-frequent-digit/find-the-least-frequent-digit.py
@@ -3,8 +3,9 @@ class Solution:
     def getLeastFrequentDigit(self, n: int) -> int:
         a = []
         b = str(n)
+        k=min(b.count(x) for x in b)
         for i in b:
-            if b.count(i) == min(b.count(x) for x in b):
+            if b.count(i) == k:
                 a.append(int(i))
         a.sort()
         return a[0]
