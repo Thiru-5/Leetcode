47dce720669c4aa9221be9e510b04e79e04410ca
Thiru-5 <165800485+Thiru-5@users.noreply.github.com>
2025-11-17 19:33:41 +0530

    Time: 13 ms (37.60%) | Memory: 21.3 MB (15.40%) - LeetSync

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.py
@@ -0,0 +1,8 @@
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                a = nums[i+1:i+k+1]
+                if 1 in a:
+                    return False
+        return True
